42e89e95fde43331e9fe5064431123648ec4740b
Riya Arora <arorariya954@gmail.com>
2026-03-08 14:17:19 +0530

    Modified files to implement authentication

--- TM.ServiceLogic/Interfaces/IAuthService.cs
+++ TM.ServiceLogic/Interfaces/IAuthService.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using TM.Contracts.Auth;
 
 namespace TM.ServiceLogic.Interfaces
 {
-    internal class IAuthService
+    public interface IAuthService
     {
+        string GenerateToken(string username, string role);
     }
 }
